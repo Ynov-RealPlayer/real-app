10fc57205547b0656972b27b0426cab10cf2adcf
MattGones <matteo.dinville@ynov.com>
2023-04-17 23:25:36 +0200

    :zap: Setup route onBoarding with authentication

--- realplayer/lib/main.dart
+++ realplayer/lib/main.dart
@@ -8,14 +8,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: OnBoardingView(),
+      debugShowCheckedModeBanner: false,
+      home: const OnBoardingView(),
       routes: {
         '/RegisterPage': (context) => RegisterPage(),
         '/LoginPage': (context) => LoginPage(),
