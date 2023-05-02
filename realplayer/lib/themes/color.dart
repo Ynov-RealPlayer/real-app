5565bb3bd0f1703cfaa59b914bacfcbfa76c402c
MattGones <matteo.dinville@ynov.com>
2023-05-02 18:45:03 +0200

    :sparkles: Add slider for categories

--- realplayer/lib/themes/color.dart
+++ realplayer/lib/themes/color.dart
@@ -3,9 +3,8 @@
 import 'package:flutter/material.dart';
 
 class ColorTheme {
-  static const Color backgroundColor = Color(0xFF12273b);
+  static const Color backgroundColor = Color(0xFF1D2B34);
   static const Color textColor = Color(0xffffffff);
   static const Color buttonColor = Color(0xF00272cd);
   static const Color searchBarColor = Color.fromARGB(157, 2, 114, 205);
-
 }
