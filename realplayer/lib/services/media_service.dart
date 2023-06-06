cd61e62f53fddcae4325ff8801a9c83dc9ae60e7
MattGones <matteo.dinville@ynov.com>
2023-06-07 00:25:20 +0200

    :sparkles: Design home page in progress without navigation

--- realplayer/lib/services/media_service.dart
+++ realplayer/lib/services/media_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:realplayer/services/auth_service.dart';
 
 class MediaService {
   static const apiUrl = "https://realplayer.fr/api";
@@ -35,6 +36,22 @@ class MediaService {
       return responseData;
     } else {
       throw Exception("Failed to post media");
+    }
+  }
+
+    static Future<Map<String, dynamic>> getMediaByCategorie(int CategorieId) async {
+    final token = await AuthService.getToken();
+    final response = await http.get(
+      Uri.parse('$apiUrl/media/category/$CategorieId'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      throw Exception("Failed to get media by categ");
     }
   }
 }
---
+++ realplayer/lib/view/AppBar.view.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:realplayer/services/category_service.dart';
+import 'package:realplayer/services/media_service.dart';
+import 'package:realplayer/themes/color.dart';
+import 'package:realplayer/view/components/NavBar.View.dart';
+
+class appBarHome extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: ColorTheme.backgroundColor,
+      elevation: 0,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/rating.svg",
+          ),
+          color: Colors.white,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            "assets/icons/notif.svg",
+          ),
+          color: Colors.white,
+        ),
+      ],
+      flexibleSpace: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 6.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Recherche',
+                    hintStyle: GoogleFonts.unicaOne(
+                      color: Colors.grey,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    filled: true,
+                    fillColor: ColorTheme.buttonColor.withOpacity(0.8),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: const Icon(Icons.search,
+                        color: Colors.grey, size: 28.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
